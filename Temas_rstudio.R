cd772da497afebbb6f2303ee4324cdb28c1906e7
Jonatan Salazar <salazarjonatan.soc@gmail.com>
2022-12-30 23:05:08 -0300

    Add script

---
+++ Temas_rstudio.R
@@ -0,0 +1,133 @@
+# ---------------------------------------------------------------------------- #
+#                Temas de RStudio rescatados desde Github
+#
+#   Cada tema contempla su nombre, url de origen y codigo para su descarga
+# ---------------------------------------------------------------------------- #
+
+
+# Lista con diversos temas ------------------------------------------------
+
+## Repositorio con diversos temas para instalar.
+
+## URL
+# https://github.com/max-alletsee/rstudio-themes
+
+
+
+
+
+# Material Ocean ----------------------------------------------------------
+
+## URL
+## https://github.com/lusignan/RStudio-Material-Theme
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/lusignan/RStudio-Material-Theme/main/material-ocean.rstheme", 
+                     apply = TRUE)
+
+
+
+
+
+# Material Dark -----------------------------------------------------------
+
+## URL
+## https://github.com/lusignan/RStudio-Material-Theme
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/lusignan/RStudio-Material-Theme/main/material-darker.rstheme", 
+                     apply = TRUE)
+
+
+
+
+
+# Mojave Dark -------------------------------------------------------------
+
+## URL
+## https://github.com/patrickshox/Mojave-Dark-RStudio-Theme
+
+## Codigo de descarga
+
+## dinamico: los graficos se ven en negro cuando no posamos el mouse sobre ellos
+rstudioapi::addTheme("https://raw.githubusercontent.com/patrickshox/Mojave-Dark-RStudio-Theme/master/Mojave%20Dark.rstheme", 
+                     apply=TRUE, force=TRUE)
+
+## Estatico: los graficos se ven como siempre
+rstudioapi::addTheme("https://raw.githubusercontent.com/patrickshox/Mojave-Dark-RStudio-Theme/master/Mojave%20Dark%20(Static).rstheme", 
+                     apply=TRUE, force=TRUE)
+
+
+
+
+
+# Wombat ------------------------------------------------------------------
+
+## URL
+## https://github.com/jealcalat/rstudio_themes
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/jealcalat/rstudio_themes/master/Wombat.rstheme", 
+                     apply = TRUE, force = TRUE)
+
+
+
+# Serendipity Dark --------------------------------------------------------
+
+## URL
+## https://github.com/gadenbuie/rsthemes
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/gadenbuie/rsthemes/main/inst/themes/serendipity-dark.rstheme", 
+                     apply = TRUE, force = TRUE)
+
+
+
+
+
+# Kiss --------------------------------------------------------------------
+
+## URL
+## https://github.com/rileytwo/kiss
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/rileytwo/kiss/master/rstudio/kiss.rstheme",
+                     apply = TRUE, force = TRUE)
+
+
+
+
+# Kandle ------------------------------------------------------------------
+
+## URL
+## https://github.com/TheMrKingent/KandleTheme
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/TheMrKingent/KandleTheme/main/inst/themes/Kandle.rstheme", 
+                     apply = TRUE, force = TRUE)
+
+
+
+
+# Poolside ----------------------------------------------------------------
+
+## URL
+## https://github.com/nsgrantham/poolside-rstudio
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/nsgrantham/poolside-rstudio/main/poolside.rstheme", 
+                     apply = TRUE, force = TRUE)
+
+
+
+
+
+# Material ----------------------------------------------------------------
+
+## URL
+## https://github.com/lusignan/RStudio-Material-Theme
+
+## Codigo de descarga
+rstudioapi::addTheme("https://raw.githubusercontent.com/lusignan/RStudio-Material-Theme/main/material.rstheme", 
+                     apply = TRUE, force = TRUE)
+
